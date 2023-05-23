8ceb50cd8aff424dfac0cf5ad9281941ba570a8f
UmidYul <yumid255@gmail.com>
2023-05-23 21:43:51 +0300

    ytfytyt

---
+++ src/components/Development.jsx
@@ -0,0 +1,20 @@
+import React, { Suspense } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Atom from "./Atom";
+import styled from "styled-components";
+
+const Development = () => {
+  return (
+    <>
+      <Canvas camera={{ position: [0, 0, 10] }}>
+        <Suspense fallback={null}>
+          <Atom />
+          <OrbitControls enableZoom={false} autoRotate />
+        </Suspense>
+      </Canvas>
+    </>
+  );
+};
+
+export default Development;
---
+++ src/components/ProductDesign.jsx
@@ -0,0 +1,22 @@
+import React, { Suspense } from "react";
+import { OrbitControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Shoe from "./Shoe";
+import styled from "styled-components";
+
+const ProductDesign = () => {
+  return (
+    <>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Stage environment="city" intensity={0.6}>
+            <Shoe />
+          </Stage>
+          <OrbitControls enableZoom={false} autoRotate />
+        </Suspense>
+      </Canvas>
+    </>
+  );
+};
+
+export default ProductDesign;
